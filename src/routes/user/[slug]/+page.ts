f75da43cb00ed926afe33cc0e7bb2bba40595edd
varun bhatia <aitvarunbhatia@gmail.com>
2023-06-30 16:45:56 +0530

    updated error

--- src/routes/user/[slug]/+page.ts
+++ src/routes/user/[slug]/+page.ts
@@ -1,8 +1,12 @@
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 export const load = (async ({ fetch, params }) => {
   const id = params.slug;
   const response = await fetch(`https://dummyjson.com/users/${id}`);
+  if (!response.ok) {
+    throw error(404, "User not found");
+  }
   let user = await response.json();
   return {
     user,
