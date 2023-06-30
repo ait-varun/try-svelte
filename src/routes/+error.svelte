51e63820d8400cf77cba3eec5b44c60a3f298209
varun bhatia <aitvarunbhatia@gmail.com>
2023-06-30 16:32:47 +0530

    updated with error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1 class="text-2xl text-red-700">
+  Oops! page loads with {$page.status}
+  {$page.error?.message}!
+</h1>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-5"
+  ><a href="/">Go Home For Users!</a></button
+>
