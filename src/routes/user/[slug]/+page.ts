5711b5f121aca1f4726849ce260469a50f76cebe
varun bhatia <aitvarunbhatia@gmail.com>
2023-06-30 17:01:57 +0530

    updated

--- src/routes/user/[slug]/+page.ts
+++ src/routes/user/[slug]/+page.ts
@@ -5,7 +5,7 @@ export const load = (async ({ fetch, params }) => {
   const id = params.slug;
   const response = await fetch(`https://dummyjson.com/users/${id}`);
   if (!response.ok) {
-    throw error(404, "User not found");
+    throw error(404, "this user is not on server");
   }
   let user = await response.json();
   return {
