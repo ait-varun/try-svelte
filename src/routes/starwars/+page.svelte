f53ba328def9bc90e9c4bb50cdb4a5e284756369
varun <aitvarunbhatia@gmail.com>
2023-08-08 10:11:42 +0530

    updated css html

--- src/routes/starwars/+page.svelte
+++ src/routes/starwars/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import { __films } from "./+page";
 
   export let allFilms: any | null = __films?.films;
@@ -18,19 +18,93 @@
 </script>
 
 <div>
+  <h1 class="title-bar">Star Wars Movies</h1>
+
   <ul>
     {#if allFilms !== null && allFilms.length > 0}
-      {#each allFilms as film}
-        <li class="px-4 py-2 text-white">
-          Title: {film?.title}, Directed By: {film?.director}
-        </li>
-        <li class="px-4 py-2 text-white">
-          Release Date: {film?.releaseDate}
-        </li>
-        <li class="px-4 py-2 text-white">
-          Species: {getSpeciesName(film?.speciesConnection)}
+      {#each allFilms as film, index}
+        <li class="film-item">
+          <div class="film-details">
+            <p>{index + 1}: {film?.title}</p>
+            <p>Directed By: {film?.director}</p>
+            <p>Release Date: {film?.releaseDate}</p>
+            <p>Species: {getSpeciesName(film?.speciesConnection)}</p>
+          </div>
         </li>
       {/each}
     {/if}
   </ul>
+</div> -->
+
+
+
+<script lang="ts">
+  import { createClient } from "../../generated/films";
+  import { onMount } from "svelte";
+
+  const client = createClient({
+    url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+  });
+
+  export let allFilms: any;
+
+  async function fetchData() {
+    const response = await client.query({
+      allFilms: {
+        films: {
+          title: true,
+          director: true,
+          releaseDate: true,
+          speciesConnection: {
+            species: {
+              name: true,
+              classification: true,
+              homeworld: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    allFilms = response.allFilms?.films;
+  }
+
+  onMount(fetchData);
+
+    function getSpeciesName(speciesConnection: any) {
+    if (
+      speciesConnection &&
+      speciesConnection.species &&
+      speciesConnection.species.length > 0
+    ) {
+      return speciesConnection.species
+        .map((species: any) => species.name)
+        .join(", ");
+    }
+    return "";
+  }
+</script>
+
+<div>
+ <h1 class="title-bar">Star Wars Movies</h1>
+  {#if typeof allFilms === 'undefined'}
+    <p class="px-4 py-2 text-white">Fetching Data...</p>
+  {:else if allFilms !== null && allFilms.length > 0}
+    <ul>
+      {#each allFilms as film, index}
+        <li class="film-item">
+          <div class="film-details">
+            <p>{index + 1}: {film?.title}</p>
+            <p>Directed By: {film?.director}</p>
+            <p>Release Date: {film?.releaseDate}</p>
+            <p>Species: {getSpeciesName(film?.speciesConnection)}</p>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p class="px-4 py-2 text-white">No data available.</p>
+  {/if}
 </div>
