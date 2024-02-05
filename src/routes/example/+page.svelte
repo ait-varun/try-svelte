ed180829202649ffd9e4d0eae2aecc4d6db5d708
varun <aitvarunbhatia@gmail.com>
2024-02-05 11:32:15 +0530

    updated type check

--- src/routes/example/+page.svelte
+++ src/routes/example/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { createClient } from "./../../generated";
+  import type { Country } from "./../../generated";
 
-  let country: Country | null = null;
+  let countries: Country | null = null;
   let isLoading = true;
 
   onMount(async () => {
@@ -27,7 +28,7 @@
       },
     });
 
-    country = response.country;
+    countries = response.country as Country;
     isLoading = false; // Set isLoading to false after fetching the data
   });
 </script>
@@ -39,17 +40,17 @@
 <div>
   {#if isLoading}
     <p class="px-4 py-2 text-white">Loading...</p>
-  {:else if country !== null && Object.keys(country).length > 0}
+  {:else if countries !== null && Object.keys(countries).length > 0}
     <!-- check if 'country' object is not empty -->
     <ul>
-      <li class="px-4 py-2 text-white">Name: {country.name}</li>
-      <li class="px-4 py-2 text-white">Native: {country.native}</li>
-      <li class="px-4 py-2 text-white">Capital: {country.capital}</li>
-      <li class="px-4 py-2 text-white">Emoji: {country.emoji}</li>
-      <li class="px-4 py-2 text-white">Currency: {country.currency}</li>
+      <li class="px-4 py-2 text-white">Name: {countries.name}</li>
+      <li class="px-4 py-2 text-white">Native: {countries.native}</li>
+      <li class="px-4 py-2 text-white">Capital: {countries.capital}</li>
+      <li class="px-4 py-2 text-white">Emoji: {countries.emoji}</li>
+      <li class="px-4 py-2 text-white">Currency: {countries.currency}</li>
       <li class="px-4 py-2 text-white">Languages:</li>
       <ul>
-        {#each country.languages as language}
+        {#each countries.languages as language}
           <!-- loop through the 'languages' array -->
           <li class="px-8 py-2 text-white">
             {language.name} - {language.code}
